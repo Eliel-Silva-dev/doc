34593cf726c515a3553822f3fca4a2b3ac62bc7c
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-10 18:45:42 -0300

    feat: doc yup js
    file yup js + install dependencies

--- index.js
+++
@@ -1,3 +0,0 @@
-const teste = () => {
-  const teste2 = '';
-};
---
+++ yup/yup.js
@@ -0,0 +1,8 @@
+/*
+Yup é um construtor de esquemas para análise e validação de valores em tempo de execução. Defina um esquema, transforme um valor para corresponder, declare a forma de um valor existente ou ambos. Os esquemas Yup são extremamente expressivos e permitem modelar validações complexas e interdependentes ou transformação de valor.
+*/
+import * as yup from 'yup';
+
+const install = 'npm i yup';
+const repositorio = 'github.com/jquense/yup';
+
