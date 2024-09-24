eca9c15bea2ec63fc88b328a8716e35df9fd92bd
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-24 19:10:30 -0300

    feat: react doc js string ex007

--- javascript/string/functions_string.js
+++ javascript/string/functions_string.js
@@ -6,3 +6,6 @@ data.toUpperCase(); //- transforma todas as letras em maiúsculas
 data.toLowerCase(); //- transforma todas as letras em minusculas
 
 data.trim(); //- tira espaços desnecessários
+
+data.padStart(nº, ''); //- preenche as strings n vezes com o valor espaço vazio nesse caso.
+data.padEnd(nº, '.'); //- preenche as strings n vezes com o valor ponto nesse caso.
