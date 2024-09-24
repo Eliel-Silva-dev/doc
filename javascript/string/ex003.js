fa5714f27a3d86faea84e478c87302b4f670eff1
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-24 18:43:51 -0300

    feat: react doc js string ex003

---
+++ javascript/string/ex003.js
@@ -0,0 +1,10 @@
+//length retorna a quantidade de caracteres
+
+let nome = 'Isabela';
+let frase = nome + ' está aprendendo Javascript';
+
+let tamanho_nome = nome.length;
+let frase_nome = frase.length;
+
+console.log(tamanho_nome); // 7
+console.log(frase_nome); // 34
