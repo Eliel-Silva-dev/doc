1905b73e6994d59594a2230f2ef96941f2340dcf
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-24 18:11:50 -0300

    feat: react doc js date loop while ex003

---
+++ javascript/loops/while/ex003.js
@@ -0,0 +1,10 @@
+let numero = 5;
+let contador = 0;
+
+console.log('Tabuada de multiplicação - Nº ' + numero);
+
+while (contador <= 10) {
+  let resultado = numero * contador;
+  console.log(numero + ' x ' + contador + ' = ' + resultado);
+  contador++;
+}
