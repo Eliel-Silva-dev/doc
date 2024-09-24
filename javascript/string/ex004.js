07097e82c3ddd59908a2e50f2e79991509aedacb
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-24 18:45:57 -0300

    feat: react doc js string ex004

---
+++ javascript/string/ex004.js
@@ -0,0 +1,9 @@
+//toUpperCase() - transforma todas as letras em maiusculas
+//toLowerCase() - transforma todas as letras em minusculas
+
+let programador_1 = 'Flávia';
+let programador_2 = 'Rogério';
+
+console.log(programador_1.toUpperCase()); // FLÁVIA
+
+console.log(programador_2.toLowerCase()); // rogério
