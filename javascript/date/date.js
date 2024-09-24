a2c83e8e92bea0abfaa1cc83a9bd0ecae291c017
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-24 13:31:04 -0300

    feat: react doc js date metodos date v3

--- javascript/date/date.js
+++ javascript/date/date.js
@@ -70,3 +70,53 @@ const personalizandoDataTempo = {
   timeStyle: 'short', // 01:05
 };
 console.log(dataAtual.toLocaleString('pt-BR', personalizandoDataTempo));
+
+//----------------------------------------
+
+//method setters
+
+dataAtual.setFullYear(1998);
+console.log(dataAtual);
+
+dataAtual.setMonth(1);
+console.log(dataAtual);
+
+dataAtual.setDate(20);
+console.log(dataAtual);
+
+dataAtual.setHours(11);
+console.log(dataAtual);
+
+dataAtual.setMinutes(0);
+console.log(dataAtual);
+
+dataAtual.setSeconds(59);
+console.log(dataAtual);
+
+dataAtual.setMilliseconds(15);
+console.log(dataAtual);
+
+//----------------------------------------------
+
+dataAtual.setUTCFullYear(1998);
+dataAtual.setUTCMonth(5);
+dataAtual.setUTCDate(20);
+dataAtual.setUTCHours(11);
+dataAtual.setUTCMinutes(0);
+dataAtual.setUTCSeconds(59);
+dataAtual.setUTCMilliseconds(15);
+
+//----------------------------------------------
+
+//uso do método setTime()
+
+console.log(dataAtual.toDateString());
+// Impressão: Mon Nov 09 2020
+
+dataAtual.setTime(886302309000);
+console.log(dataAtual.toDateString());
+// Impressão: Sun Feb 01 1998
+
+//Os métodos setters do objeto Date alteram uma parte específica da data
+
+//----------------------------------------------
