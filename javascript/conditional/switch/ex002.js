48676aa941265cee906bde437ded5e9bd915c039
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-24 12:35:34 -0300

    feat: react doc js conditional switch ex002

---
+++ javascript/conditional/switch/ex002.js
@@ -0,0 +1,33 @@
+let data = new Date();
+
+const tipo_mes = {
+  month: 'long',
+};
+
+let mes = data.toLocaleString('pt-br', tipo_mes);
+
+switch (mes) {
+  case 'Janeiro':
+  case 'Fevereiro':
+  case 'Março':
+    console.log('Estamos na estação do Verão');
+    break;
+  case 'abril':
+  case 'Maio':
+  case 'Junho':
+    console.log('Estamos na estação do Outono');
+    break;
+  case 'Julho':
+  case 'Agosto':
+  case 'Setembro':
+    console.log('Estamos na estação do Inverno');
+    break;
+  case 'Outubro':
+  case 'Novembro':
+  case 'dezembro':
+    console.log('Estamos na estação do Primavera');
+    break;
+  default:
+    console.log('Mês inválido, não foi possível encontrar a estação');
+    break;
+}
