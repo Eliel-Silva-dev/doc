557b9a5abd9561712c807fe2e380d018f02eef36
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-24 18:11:03 -0300

    feat: react doc js date loop while ex002

---
+++ javascript/loops/while/ex002.js
@@ -0,0 +1,7 @@
+let contador = 0; // contador iniciado em 0
+
+while (contador < 10) {
+  // enquanto contrador menor que 5 faça:
+  console.log('O valor da variavel contador é: ' + contador); // imprima o conteudo da varial contador.
+  contador++; // incremente a variavel a cada inteiração.
+} // apos executar o bloco de comandos ele retorna ao inicio
