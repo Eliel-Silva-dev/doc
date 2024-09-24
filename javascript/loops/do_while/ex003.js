d483217e438117afa15249ab6b8a3e43ec5f1430
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-24 13:38:35 -0300

    feat: react doc js date loop do-while ex003

---
+++ javascript/loops/do_while/ex003.js
@@ -0,0 +1,23 @@
+let contador = 0;
+
+let meses = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+];
+
+do {
+  //execulta o codigo polomenos 1 vez
+  console.log(meses[contador]);
+
+  contador++;
+} while (contador < meses.length);
