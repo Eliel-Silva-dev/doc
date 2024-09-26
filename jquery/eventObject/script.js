d468e2eec4f5644844cfcce8567333e54ef3f3e4
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-26 12:05:52 -0300

    feat: react doc jquery event object

---
+++ jquery/eventObject/script.js
@@ -0,0 +1,21 @@
+let pmx = 0;
+let pmy = 0;
+
+$(document).mousemove((event) => {
+  pmx = event.pageX;
+  pmy = event.pageY;
+
+  if (pmx > 500) {
+    console.log('Posição x maior que 500px');
+  }
+
+  $('#text').text(`A posição do x é: ${pmx} px
+    e a posição do y é: ${pmy} px`);
+});
+
+$(document).click((event) => {
+  $('#res').text(`O item clicado foi: ${event.target.tagName}`);
+  if (event.target.id == 'res') {
+    alert('Não pode ser clicado!');
+  }
+});
