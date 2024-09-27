9b36dc2f759df1c4b3f4df4054810e87a645f3d4
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-27 17:47:18 -0300

    feat: doc python calculator

---
+++ python/projects/calculatorTinta/calculator_mod.py
@@ -0,0 +1,14 @@
+class Calculadora:
+    __area_parede: float
+    __area_teto: float
+
+    def calcular_area_paredes(self, comodo):
+        self.__area_parede = 2 * (comodo.largura + comodo.profundidade) * comodo.altura
+        return self.__area_parede
+
+    def calcular_area_teto(self, comodo):
+        self.__area_teto = comodo.largura * comodo.profundidade
+        return self.__area_teto
+
+    def calcular_litragem_necessaria(self):
+        return (self.__area_parede + self.__area_teto) / 10
---
+++ python/projects/calculatorTinta/main.py
@@ -0,0 +1,21 @@
+from calculadora_mod import Calculadora
+calc = Calculadora()
+
+from comodo import Comodo
+comodo = Comodo(input('Qual a largura do comodo? '), input('Qual a profundidade do comodo? '))
+
+#calc.area_parede = 2 * (largura + profundidade) * altura
+
+print('A area das paredes é: ',
+      calc.calcular_area_paredes(comodo), 'M²')
+
+#calc.area_teto = largura * altura
+
+print('A area do teto é: ',
+      calc.calcular_area_teto(comodo), 'M²')
+
+print('A litragem de tint necessaria é: ',
+      calc.calcular_litragem_necessaria(), 'M²'
+      )
+
+# aula 7
