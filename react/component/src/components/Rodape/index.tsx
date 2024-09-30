4da8abc2c7d1b00aa11c3b5e89bbc38a43c4d9ba
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-29 21:43:30 -0300

    feat: react component rodape

---
+++ react/component/src/components/Rodape/index.tsx
@@ -0,0 +1,9 @@
+import style from './rodape.module.css';
+
+export const Rodape = () => {
+  return (
+    <footer className={style.container_footer}>
+      <h2>Desenvolvido por Eliel Silva</h2>
+    </footer>
+  );
+};
