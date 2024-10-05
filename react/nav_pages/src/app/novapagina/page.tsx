d6da19a4978ca7a6adc62fe0b83226459578a100
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-10-05 16:36:43 -0300

    feat: react nav_react nova page

---
+++ react/nav_pages/src/app/novapagina/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import style from './style.module.css';
+
+export default function NovaPagina() {
+  return (
+    <main className={style.main_container}>
+      <div className={style.card_container}>
+        <h1>Nova página</h1>
+        <p>Éssa é a nova pagina da aplicação</p>
+        <p>Éssa é nossa primeira página criada no react</p>
+        <Link href="/">Pagina inicial</Link>
+      </div>
+    </main>
+  );
+}
