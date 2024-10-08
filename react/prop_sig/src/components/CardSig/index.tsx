297b66104dca49b2a4f4b4ab37112647e031ec73
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-10-08 19:31:59 -0300

    feat: react propSig component cardsig

---
+++ react/prop_sig/src/components/CardSig/index.tsx
@@ -0,0 +1,28 @@
+import style from './CardSigno.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+type TPropSig = {
+  signo: string;
+  image: string;
+  dataFim: string;
+  dataInicio: string;
+  link: string;
+};
+
+export default function CardSigno(props: TPropSig) {
+  return (
+    <div className={style.card}>
+      <h3>{props.signo}</h3>
+      <Image src={props.image} width="100" height="100" alt="" />
+      <p>
+        As pessoas que nascem entre <strong>{props.dataInicio}</strong> -{' '}
+        <strong>{props.dataFim}</strong> são classificados como{' '}
+        <strong>{props.signo}</strong>
+      </p>
+      <Link className={style.link_info} href={props.link}>
+        Saiba mais
+      </Link>
+    </div>
+  );
+}
