f629b920ce74b0f5af6075d064d4b5b2c4ffcc0a
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-10-11 17:04:34 -0300

    feat: react propSig page cancer

---
+++ react/prop_sig/src/app/cancer/page.tsx
@@ -0,0 +1,24 @@
+import style from './Cancer.module.css';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Cancer() {
+  return (
+    <main className={style.main_cancer}>
+      <div className={style.page}>
+        <h1>Cancer</h1>
+        <Image src="/cancer.jpg" width="300" height="300" alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+          obcaecati excepturi. Aspernatur dolores ea vel temporibus, ipsum
+          distinctio modi. Quae quibusdam nobis architecto quo aut corrupti sint
+          aliquam nostrum neque!
+        </p>
+        <Link className="link_home" href="/">
+          Voltar
+        </Link>
+      </div>
+    </main>
+  );
+}
