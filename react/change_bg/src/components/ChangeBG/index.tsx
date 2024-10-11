8c612466cf3a9c305f1dc7fed8fc0e7bae8cd578
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-10-11 17:44:30 -0300

    feat: react change bg component ajuste de import

--- react/change_bg/src/components/ChangeBG/index.tsx
+++ react/change_bg/src/components/ChangeBG/index.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { useState } from 'react';
-import style from './Troca_bg.module.css';
+import style from './style.module.css';
 
 export const ChangeBG = () => {
   let [nova_bg, setNova_bg] = useState(style.bg_vermelho);
