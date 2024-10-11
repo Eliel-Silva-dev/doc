3fa1865fc2691dfb849e9aea632bf7059f2d2d97
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-10-10 21:20:35 -0300

    feat: react propSig component footer

---
+++ react/prop_sig/src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import style from './style.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={style.footer}>
+      <p>Desenvolvido por Eliel Silva</p>
+    </footer>
+  );
+}
