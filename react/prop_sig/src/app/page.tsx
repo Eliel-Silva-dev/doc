fad52374ab20b237c08c7f673d4d43e920c8b5b4
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-10-11 16:55:00 -0300

    feat: react propSig page home

--- react/prop_sig/src/app/page.tsx
+++ react/prop_sig/src/app/page.tsx
@@ -1,95 +1,93 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
+import CardSigno from '@/components/CardSig';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className={styles.main}>
+      <CardSigno
+        signo="Aquário"
+        image="/Aquario.jpg"
+        dataInicio="21/01"
+        dataFim="19/02"
+        link="/aquario"
+      />
+      <CardSigno
+        signo="Peixes"
+        image="/peixes.jpg"
+        dataInicio="20/02"
+        dataFim="20/03"
+        link="/peixes"
+      />
+      <CardSigno
+        signo="Áries"
+        dataInicio="21/03"
+        dataFim="20/04"
+        image="/aries.jpg"
+        link="/aries"
+      />
+      <CardSigno
+        signo="Touro"
+        dataInicio="21/04"
+        dataFim="21/05"
+        image="/touro.jpg"
+        link="/touro"
+      />
+      <CardSigno
+        signo="Gêmeos"
+        dataInicio=" 22/05"
+        dataFim="21/06"
+        image="/gemeos.jpg"
+        link="/gemeos"
+      />
+      <CardSigno
+        signo="Câncer"
+        dataInicio="21/06"
+        dataFim="23/07"
+        image="/cancer.jpg"
+        link="/cancer"
+      />
+      <CardSigno
+        signo="Leão"
+        dataInicio="24/07"
+        dataFim="23/08"
+        image="/leao.jpg"
+        link="/leao"
+      />
+      <CardSigno
+        signo="Virgem"
+        dataInicio="24/08"
+        dataFim="23/09"
+        image="/virgem.jpg"
+        link="/virgem"
+      />
+      <CardSigno
+        signo="Libra"
+        dataInicio="24/09"
+        dataFim="23/10"
+        image="/libra.jpg"
+        link="/libra"
+      />
+      <CardSigno
+        signo="Escorpião"
+        dataInicio="24/10"
+        dataFim="22/11"
+        image="/escorpiao.jpg"
+        link="/escorpiao"
+      />
+      <CardSigno
+        signo="Sagitário"
+        dataInicio="23/11"
+        dataFim="21/12"
+        image="/sagitario.jpg"
+        link="/sagitario"
+      />
+      <CardSigno
+        signo="Capricórnio"
+        dataInicio="22/12"
+        dataFim="20/01"
+        image="/capricornio.jpg"
+        link="/capricornio"
+      />
+    </main>
   );
 }
